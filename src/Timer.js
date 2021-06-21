df7d34679e56b72670a38de70b3dad13d0bae712
AllisonAnz <AllisonAnzalone@gmail.com>
2021-06-21 11:02:02 -0500

    Completed Assignment

--- src/Timer.js
+++ src/Timer.js
@@ -7,6 +7,21 @@ class Timer extends Component {
   };
 
   // add your code here
+  //ComponentDidMount is good place to include setinterval or setTImeout function
+  //this allows you to delay something from happening on a component or cause some repeating change
+  //To create a setInterval assign it to a variable withing the scope of our class 
+  //Write a CDM that initalizes the interval, pass clockTick as the callback function 
+  //and set it to 1000 to update every second
+  componentDidMount(){
+    this.interval = setInterval(this.clockTick, 1000)
+    //When a new timer is added, we should see the displayed number increase every second
+  }
+
+  //We need to clear an interval, use build in clearInterval passing in the local var.
+  //Write a componentWillUnmount method that cleans up the interval you've created 
+  componentWillUnmount(){
+    clearInterval(this.interval)
+  }
 
   render() {
     const { time, color } = this.state;
@@ -21,6 +36,8 @@ class Timer extends Component {
   }
 
   //clock functions
+  //this method handles updating the state value (time) and is then included in the render
+  //we need to set up an interval to call clockTick
   clockTick = () => {
     this.setState(prevState => ({
       time: prevState.time + 1
